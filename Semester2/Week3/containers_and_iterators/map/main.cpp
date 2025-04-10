2f7454f1752524b26aec4e4a9a919a64f75538d4
rayzedan <bezlepkin_d01@mail.ru>
2025-04-10 22:27:23 +0500

    deleted executables

--- Semester2/Week3/containers_and_iterators/map/main.cpp
+++ Semester2/Week3/containers_and_iterators/map/main.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
 #include <map>
 #include <string>
 
 int main()
 {
     std::map<std::string, int> map = { {"fisrt", 1} };
+    auto it = map.find("first");
+    if (it != map.end())
+    {
+        // Bad pattern (2 operations vs 1 operation)
+        map["first"] = 2;
+        // Good variant
+        it->second = 2;
+    }
 }
